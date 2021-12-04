795a74f9ad253b66af804b7ca8c6bd9b7cf217b9
Mathieu Niord <69221076+MathieuNiord@users.noreply.github.com>
2021-12-04 17:01:39 +0100

    Project base

---
+++ Yahzee_cpp/Test_Yahtzee/Test_Yahtzee.cpp
@@ -0,0 +1,22 @@
+/* Authors : Frejoux G., Niord M. */
+
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test_yahtzee {
+
+	TEST_CLASS(Test_Yahtzee) {
+
+	public:
+		
+		TEST_METHOD(TestMethod1) {}
+		TEST_METHOD(TestMethod2) {}
+		TEST_METHOD(TestMethod3) {}
+		TEST_METHOD(TestMethod4) {}
+		TEST_METHOD(TestMethod5) {}
+		TEST_METHOD(TestMethod6) {}
+
+	};
+}
