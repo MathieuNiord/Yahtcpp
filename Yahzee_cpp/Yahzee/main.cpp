1d9b28ea1edcbd5c8a4facbe0a862a477dda3557
Frejoux <gaetanfrejoux@gmail.com>
2021-12-10 18:21:04 +0100

    Asking how many players

--- Yahzee_cpp/Yahzee/main.cpp
+++ Yahzee_cpp/Yahzee/main.cpp
@@ -4,8 +4,13 @@
 
 int main(int argc, char* argv[]) {
 
-	std::cout << "Hello world!" << std:: endl;
+	int nb_player;
 
+	std::cout << "==Yahtzee==" << std:: endl;
+	std::cout << "Please, how many players : ";
+	std::cin >> nb_player;
+
+	//TODO call game constructor with nb_player
 
 	return EXIT_SUCCESS;
 
