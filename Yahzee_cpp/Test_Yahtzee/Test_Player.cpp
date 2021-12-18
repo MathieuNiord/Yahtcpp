6cae8b3e73e29438a07eccbeaab08f0e9b82718e
MathieuN <mathieu.niord@etu.univ-poitiers.fr>
2021-12-18 17:25:49 +0100

    fichiers de tests créés

---
+++ Yahzee_cpp/Test_Yahtzee/Test_Player.cpp
@@ -0,0 +1,49 @@
+/* Authors : Frejoux G., Niord M. */
+
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include <iostream>
+#include <vector>
+
+#include "../Yahzee/figure.h"
+#include "../Yahzee/fig_superior.h"
+#include "../Yahzee/many_of_kind.h"
+#include "../Yahzee/full_house.h"
+#include "../Yahzee/straight.h"
+#include "../Yahzee/yahtzee.h"
+#include "score.h"
+#include "player.h"
+
+//namespace coo_yahtzee {
+//	class figure;
+//	template<const int Value> class fig_superior;
+//	template<const int Number> class many_of_kind;
+//	class full_house;
+//	template<const int Number> class straight;
+//	class yahtzee;
+//}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace coo_yahtzee;
+
+namespace test_yahtzee {
+
+	TEST_CLASS(test_player) {
+
+public:
+
+	TEST_METHOD(test_player_init) {
+
+	}
+
+	TEST_METHOD(test_player_advertise) {
+
+	}
+
+	TEST_METHOD(test_eliminate) {
+
+	}
+
+	};
+}
