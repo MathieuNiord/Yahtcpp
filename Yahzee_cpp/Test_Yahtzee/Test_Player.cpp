e8d427a11a7acf9d06ebb5cb1baf9b301cf95e24
Frejoux <gaetanfrejoux@gmail.com>
2021-12-19 22:13:53 +0100

    ajout test player

--- Yahzee_cpp/Test_Yahtzee/Test_Player.cpp
+++ Yahzee_cpp/Test_Yahtzee/Test_Player.cpp
@@ -1,11 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
-#include "../Yahtzee_COO/resources.h"
-#include "../Yahtzee_COO/score.h"
 #include "../Yahtzee_COO/player.h"
 #include "../Yahtzee_COO/ai_player.h"
-#include "../Yahtzee_COO/score.cpp"
 #include "../Yahtzee_COO/player.cpp"
 #include "../Yahtzee_COO/ai_player.cpp"
 
@@ -15,41 +12,90 @@ namespace coo_yahtzee {
 
 	TEST_CLASS(Test_Player) {
 
-public:
+	public:
 
-	const std::string& player_name = "Player";
-	const std::string& bot_name = "Bot";
+		const std::string& player_name = "Player";
+		const std::string& bot_name = "Bot";
 
-	TEST_METHOD(test_player_init) {
+		TEST_METHOD(test_player_init) {
 
-		// Initialization
-		const player* player_ = new player(player_name);
+			// Initialization
+			const player* player_ = new player(player_name);
 
-		// Assertions
-		Assert::IsNotNull(player_);
-		Assert::AreEqual(0, player_->advertise_count_);
-		Assert::IsFalse(player_->eliminated_);
-		Assert::AreEqual(player_name, player_->c_name_);
+			// Assertions
+			Assert::IsNotNull(player_);
+			Assert::AreEqual(0, player_->advertise_count_);
+			Assert::IsFalse(player_->eliminated_);
+			Assert::AreEqual(player_name, player_->c_name_);
 
-		// Destruction
-		delete player_;
-	}
+			// Destruction
+			delete player_;
+		}
 
-	TEST_METHOD(test_bot_init) {
+		TEST_METHOD(test_bot_init) {
 
-		// Initialization
-		const ai_player* bot_ = new ai_player(bot_name);
+			// Initialization
+			const ai_player* bot_ = new ai_player(bot_name);
 
-		// Assertions
-		Assert::IsNotNull(bot_);
-		Assert::AreEqual(bot_name, bot_->c_name_);
+			// Assertions
+			Assert::IsNotNull(bot_);
+			Assert::AreEqual(bot_name, bot_->c_name_);
 
-		// Destruction
-		delete bot_;
-	}
+			// Destruction
+			delete bot_;
+		}
 
-	TEST_METHOD(test_advertise_player) {
-	}
+		TEST_METHOD(test_advertise_player) {
 
+			// Initialization
+			player* player_ = new player(player_name);
+
+			// Assertions
+			Assert::AreEqual(0, player_->advertise_count_);
+
+			player_->advertise_player("0");
+			Assert::AreEqual(0, player_->advertise_count_);
+
+			player_->advertise_player("advertise");
+			Assert::AreEqual(1, player_->advertise_count_);
+
+			player_->advertise_player("advertise");
+			player_->advertise_player("advertise");
+			Assert::AreEqual(3, player_->advertise_count_);
+
+			// Destruction
+			delete player_;
+		}
+
+		TEST_METHOD(test_eliminate_player) {
+
+			// Initialization
+			player* player_ = new player(player_name);
+
+			// Assertions
+			Assert::IsFalse(player_->eliminated_);
+
+			player_->eliminate();
+
+			Assert::IsTrue(player_->eliminated_);
+
+			// Destruction
+			delete player_;
+		}
+
+
+		TEST_METHOD(test_score_result) {
+
+			// Initialization
+			player* player_ = new player(player_name);
+			player_->score_all();
+
+			// Assertions
+			Assert::AreEqual(0, player_->get_score_result());
+
+			
+			// Destruction
+			delete player_;
+		}
 	};
 }
